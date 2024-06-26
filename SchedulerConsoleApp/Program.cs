f28d05f43dcf3cf6697c1a0baa9d1e05b4c4a392
jason <jason>
2024-06-26 19:56:32 +0300

    Add project files.

---
+++ SchedulerConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+    }
+    private void Print(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
